bd28c236262f0c5342aac8ee05faf71170cecd1f
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-23 12:38:32 +0300

    feat(core-database): add ArtistEntity data class
    ArtistEntity represents song artist information.

---
+++ core/database/src/main/java/com/kire/database/entity/ArtistEntity.kt
@@ -0,0 +1,16 @@
+package com.kire.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Represents song artist information
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+@Entity(tableName = "artists")
+data class ArtistEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String
+)
