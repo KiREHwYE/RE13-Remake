8abb08e4713913ff16ec45cd1c1376bbdee75c80
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-23 12:40:49 +0300

    feat(core-database): add AlbumEntity data class
    AlbumEntity represents album information including fields for title, artist and artwork uri.

---
+++ core/database/src/main/java/com/kire/database/entity/AlbumEntity.kt
@@ -0,0 +1,18 @@
+package com.kire.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Represents album information, including fields for title, artist and artwork uri.
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+@Entity(tableName = "albums")
+data class AlbumEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val artistId: Long?,
+    val artworkUri: String?
+)
