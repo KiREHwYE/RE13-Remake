bc3e40bbd218cb9eacce9c2ad14c4340af59f313
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-23 12:42:43 +0300

    feat(core-database): add PlaylistEntity data class
    PlaylistEntity represents information about playlist created by user

---
+++ core/database/src/main/java/com/kire/database/entity/PlaylistEntity.kt
@@ -0,0 +1,16 @@
+package com.kire.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Represents information about playlist created by user
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+@Entity(tableName = "playlists")
+data class PlaylistEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String
+)
