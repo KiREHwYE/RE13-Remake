5cbf9fbdb0420784534920cfd46a50c2b3e44443
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-24 16:43:03 +0300

    feat(app): add ExtendedTheme usage

--- app/src/main/java/com/kire/re13remastered/MainActivity.kt
+++ app/src/main/java/com/kire/re13remastered/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.kire.ui.theme.RE13RemasteredTheme
+import com.kire.ui.theme.RE13RemakeExtendedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            com.kire.ui.theme.RE13RemasteredTheme {
+            RE13RemakeExtendedTheme {
 
             }
         }
