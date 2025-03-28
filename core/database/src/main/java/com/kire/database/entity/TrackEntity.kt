a459c9cdf729d306d298c831264a15ca3ac9f73c
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-28 18:03:10 +0300

    docs(core:database): enhance TrackEntity documentation with detailed param descriptions

--- core/database/src/main/java/com/kire/database/entity/TrackEntity.kt
+++ core/database/src/main/java/com/kire/database/entity/TrackEntity.kt
@@ -1,10 +1,21 @@
 package com.kire.database.entity
 
-import android.net.Uri
 import androidx.room.Entity
 
 /**
  * Represents song information, including fields for title, artist, album, etc.
+ *
+ * @param id Unique identifier of the audio file. Defaults to 0 if not specified.
+ * @param artistId Identifier of the artist. Nullable and defaults to 0 if unknown.
+ * @param albumId Identifier of the album. Nullable and defaults to 0 if unknown.
+ * @param title Title of the audio file.
+ * @param lyrics Lyrics of the audio file, nullable and defaults to null.
+ * @param duration Duration in milliseconds.
+ * @param dateModified Last modification timestamp in seconds since epoch.
+ * @param bitrate Bitrate in bits per second.
+ * @param size Size in bytes.
+ * @param data URI in String format to the audio file's location.
+ * @param artworkUri URI in String format to the album artwork.
  *
  * @author Michael Gontarev (KiREHwYE)
  */
