2334a2a9cd32a33dec81c439c1f0dc9d16d68c38
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-28 18:05:45 +0300

    docs(core:database): enhance AlbumEntity documentation with detailed param descriptions

--- core/database/src/main/java/com/kire/database/entity/AlbumEntity.kt
+++ core/database/src/main/java/com/kire/database/entity/AlbumEntity.kt
@@ -6,6 +6,11 @@ import androidx.room.PrimaryKey
 /**
  * Represents album information, including fields for title, artist and artwork uri.
  *
+ * @param id Unique identifier of album. Defaults to 0 if not specified.
+ * @param artistId Identifier of the artist. Nullable and defaults to 0 if unknown.
+ * @param title Title of the audio file.
+ * @param artworkUri URI in String format to the album artwork.
+ *
  * @author Michael Gontarev (KiREHwYE)
  */
 @Entity(tableName = "albums")
