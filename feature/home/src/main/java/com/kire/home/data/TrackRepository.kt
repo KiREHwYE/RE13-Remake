4bd2c37b6ab2d44afa58dd6fe8d6bd1839d215a1
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-28 19:53:37 +0300

    feat(feature:home): add TrackRepository implementation for ITrackRepository
    Implemented TrackRepository to provide database operations for tracks via TrackDao.

---
+++ feature/home/src/main/java/com/kire/home/data/TrackRepository.kt
@@ -0,0 +1,66 @@
+package com.kire.home.data
+
+import com.kire.database.dao.TrackDao
+import com.kire.di.IODispatcher
+import com.kire.home.data.mapper.to_domain.toDomain
+import com.kire.home.data.mapper.to_entity.toEntity
+import com.kire.home.domain.model.TrackDomain
+import com.kire.home.domain.repository.ITrackRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+/**
+ * Repository implementation for managing track-related data in the database.
+ * This class implements [ITrackRepository] to provide concrete functionality for upserting, deleting,
+ * and retrieving tracks, using [TrackDao] for database operations.
+ * Utilizes [CoroutineDispatcher] to ensure I/O-bound tasks are performed off the main thread.
+ *
+ * @param trackDao The [TrackDao] instance for interacting with the track database table, injected via Dagger.
+ * @param coroutineDispatcher The [CoroutineDispatcher] for executing database operations, annotated with
+ *                            [@IODispatcher] to specify an I/O-optimized thread pool, injected via Dagger.
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+class TrackRepository @Inject constructor(
+    private val trackDao: TrackDao,
+    @IODispatcher private val coroutineDispatcher: CoroutineDispatcher
+) : ITrackRepository {
+
+    /**
+     * Inserts or updates a track entity in the database.
+     * This suspend function converts the provided [TrackDomain] to a [TrackEntity] using [TrackDomain.toEntity]
+     * and delegates the upsert operation to [TrackDao.upsertTrack].
+     *
+     * @param track The [TrackDomain] to upsert.
+     */
+    override suspend fun upsertTrack(track: TrackDomain) {
+        trackDao.upsertTrack(track.toEntity())
+    }
+
+    /**
+     * Deletes a track entity from the database.
+     * This suspend function converts the provided [TrackDomain] to a [TrackEntity] using [TrackDomain.toEntity]
+     * and delegates the deletion to [TrackDao.deleteTrack].
+     *
+     * @param track The [TrackDomain] to delete.
+     */
+    override suspend fun deleteTrack(track: TrackDomain) {
+        trackDao.deleteTrack(track.toEntity())
+    }
+
+    /**
+     * Retrieves all track entities from the database as a Flow.
+     * This suspend function fetches a [Flow] of [TrackEntity] lists from [TrackDao.getAllTracks],
+     * maps each list to a list of [TrackDomain] objects using [List<TrackEntity>.toDomain], and returns
+     * the resulting [Flow].
+     *
+     * @return A [Flow] emitting a list of all [TrackDomain] objects in the database.
+     */
+    override suspend fun getAllTracks(): Flow<List<TrackDomain>> {
+        return trackDao.getAllTracks().map {
+            it.toDomain()
+        }
+    }
+}
