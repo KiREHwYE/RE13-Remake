0fa185071543ab85bec410386b479104cb39ae4a
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-28 19:41:53 +0300

    feat(feature:home): add List<TrackEntity>.toDomain extension for batch data-to-domain mapping
    Introduced toDomain extension function for List<TrackEntity>, enabling conversion to a list of TrackDomain objects by leveraging TrackEntity.toDomain.

---
+++ feature/home/src/main/java/com/kire/home/data/mapper/ListTrackEntityToDomain.kt
@@ -0,0 +1,18 @@
+package com.kire.home.data.mapper
+
+import com.kire.database.entity.TrackEntity
+
+/**
+ * Converts a list of [TrackEntity] objects from the data layer into a list of [TrackDomain] objects for the domain layer.
+ * This extension function applies the [TrackEntity.toDomain] mapping to each element in the list,
+ * providing a convenient way to transform an entire collection of database entities into platform-independent
+ * domain models for use in business logic.
+ *
+ * @receiver The list of [TrackEntity] instances to be converted.
+ * @return A list of [TrackDomain] instances, each mapped from a corresponding [TrackEntity] in this list.
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+fun List<TrackEntity>.toDomain() = this.map { trackEntity ->
+    trackEntity.toDomain()
+}
