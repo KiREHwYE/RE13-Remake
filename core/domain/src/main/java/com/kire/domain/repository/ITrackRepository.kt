ff8e8b1ef34aa1804494f2dc8a0431bbc5f7c152
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-28 18:33:37 +0300

    feat(core:domain): add ITrackRepository interface for track management
    Introduced ITrackRepository interface in the domain layer to define track-related operations,
    including upsert, delete, get by ID, and get all tracks.

---
+++ core/domain/src/main/java/com/kire/domain/repository/ITrackRepository.kt
@@ -0,0 +1,42 @@
+package com.kire.domain.repository
+
+import com.kire.domain.model.TrackDomain
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository for managing track-related data.
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+interface ITrackRepository {
+
+    /**
+     * Inserts or updates a track entity in the database.
+     * If the track already exists, it will be updated; otherwise, it will be inserted.
+     *
+     * @param track The [TrackDomain] to upsert.
+     */
+    suspend fun upsertTrack(track: TrackDomain)
+
+    /**
+     * Deletes a track entity from the database.
+     *
+     * @param track The [TrackDomain] to delete.
+     */
+    suspend fun deleteTrack(track: TrackDomain)
+
+    /**
+     * Retrieves a track entity by its unique identifier.
+     *
+     * @param id The unique identifier of the track.
+     * @return The [TrackDomain] with the specified [id], or null if no such track exists.
+     */
+    suspend fun getTrackById(id: Int): TrackDomain
+
+    /**
+     * Retrieves all track entities from the database as a Flow.
+     *
+     * @return A [Flow] emitting a list of all [TrackDomain] objects in the database.
+     */
+    suspend fun getAllTracks(): Flow<List<TrackDomain>>
+}
