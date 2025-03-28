16cd69d44cf9d96eb2e14ac4b77f4b0a64ea7b43
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-28 19:36:31 +0300

    feat(core:domain): add TrackEntity.toDomain extension for data-to-domain mapping

---
+++ feature/home/src/main/java/com/kire/home/data/mapper/TrackEntityToDomain.kt
@@ -0,0 +1,29 @@
+package com.kire.home.data.mapper
+
+import com.kire.database.entity.TrackEntity
+import com.kire.home.domain.model.TrackDomain
+
+/**
+ * Converts a [TrackEntity] from the data layer into a [TrackDomain] for the domain layer.
+ * This extension function maps all fields of the [TrackEntity] to a new [TrackDomain] instance,
+ * ensuring a seamless transition between the database representation and the platform-independent
+ * domain model used for business logic.
+ *
+ * @receiver The [TrackEntity] instance to be converted.
+ * @return A [TrackDomain] instance with fields mapped from this [TrackEntity].
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+fun TrackEntity.toDomain() = TrackDomain(
+    id = this.id,
+    artistId = this.artistId,
+    albumId = this.albumId,
+    title = this.title,
+    lyrics = this.lyrics,
+    duration = this.duration,
+    dateModified = this.dateModified,
+    bitrate = this.bitrate,
+    size = this.size,
+    data = this.data,
+    artworkUri = this.artworkUri
+)
