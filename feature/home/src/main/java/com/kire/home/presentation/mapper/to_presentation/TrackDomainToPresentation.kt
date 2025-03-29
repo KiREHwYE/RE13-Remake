7f6553a8e5a98579f66c366743b3b077c658bcf3
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-29 17:18:42 +0300

    feat(feature:home): add mapper for TrackDomain.
    Converts a TrackDomain from the domain layer into a Track for the presentation layer.

---
+++ feature/home/src/main/java/com/kire/home/presentation/mapper/to_presentation/TrackDomainToPresentation.kt
@@ -0,0 +1,27 @@
+package com.kire.home.presentation.mapper.to_presentation
+
+import com.kire.home.domain.model.TrackDomain
+import com.kire.home.presentation.model.Track
+
+/**
+ * Converts a [TrackDomain] from the domain layer into a [Track] for the presentation layer.
+ * This extension function maps all fields of the [TrackDomain] to a new [Track] instance.
+ *
+ * @receiver The [TrackDomain] instance to be converted.
+ * @return A [Track] instance with fields mapped from this [TrackDomain].
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+fun TrackDomain.toPresentation() = Track(
+    id = this.id,
+    artistId = this.artistId,
+    albumId = this.albumId,
+    title = this.title,
+    lyrics = this.lyrics,
+    duration = this.duration,
+    dateModified = this.dateModified,
+    bitrate = this.bitrate,
+    size = this.size,
+    data = this.data,
+    artworkUri = this.artworkUri
+)
