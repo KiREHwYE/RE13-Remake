d416ac1a1a3c53c6717b5a3bddcf41ca80f7eb23
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-29 17:54:11 +0300

    refactor(GetAllTracksUseCase): remove unnecessary suspend.

--- feature/home/src/main/java/com/kire/home/domain/usecase/GetAllTracksUseCase.kt
+++ feature/home/src/main/java/com/kire/home/domain/usecase/GetAllTracksUseCase.kt
@@ -6,8 +6,7 @@ import javax.inject.Inject
 /**
  * Use case for retrieving all tracks from the database.
  * This class encapsulates the logic for fetching all available tracks as a [Flow] of list filled with [TrackDomain] objects,
- * delegating the data retrieval to the provided [ITrackRepository]. It is designed to be invoked asynchronously
- * within a coroutine scope with [Dispatchers.IO]
+ * delegating the data retrieval to the provided [ITrackRepository].
  *
  * @param trackRepository The [ITrackRepository] instance used to access track data, injected via Dagger.
  */
@@ -16,11 +15,11 @@ class GetAllTracksUseCase @Inject constructor(
 ) {
     /**
      * Retrieves all tracks from the database as a Flow.
-     * This suspend operator function invokes the underlying [ITrackRepository.getAllTracks] method,
+     * This operator function invokes the underlying [ITrackRepository.getAllTracks] method,
      * returning a [Flow] that emits a list of [TrackDomain] objects representing all tracks stored in the database.
      *
      * @return A [Flow] emitting a list of [TrackDomain] objects.
      */
-    suspend operator fun invoke() =
+     operator fun invoke() =
         trackRepository.getAllTracks()
 }
