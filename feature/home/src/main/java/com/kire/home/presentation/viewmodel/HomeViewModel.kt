430acf310c5b2a5e71b5d0e128fcc20af4bcefcd
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-30 12:57:26 +0300

    feat(feature:home): add HomeViewModel for managing track state in UI
    Introduced HomeViewModel to handle track data for the home feature UI, utilizing ITrackUseCases for track-related operations.

---
+++ feature/home/src/main/java/com/kire/home/presentation/viewmodel/HomeViewModel.kt
@@ -0,0 +1,58 @@
+package com.kire.home.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kire.home.domain.usecase.ITrackUseCases
+import com.kire.home.presentation.mapper.to_presentation.toPresentation
+import com.kire.home.presentation.model.Track
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * ViewModel for the home feature, managing the state of track data for the UI.
+ * This class uses [ITrackUseCases] for track-related operations. It leverages Dagger for dependency injection
+ * and initializes track retrieval upon creation, ensuring the UI stays updated with the latest track list.
+ *
+ * @param trackUseCases The [ITrackUseCases] instance providing track-related use cases, injected via Dagger.
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+class HomeViewModel @Inject constructor(
+    private val trackUseCases: ITrackUseCases
+) : ViewModel() {
+
+    /**
+     * Mutable state flow holding the current list of tracks for internal updates.
+     * Initialized with an empty list, this flow is updated with track data fetched from [ITrackUseCases].
+     */
+    private val _tracks: MutableStateFlow<List<Track>> = MutableStateFlow(emptyList())
+
+    /**
+     * Public state flow exposing the list of tracks for UI observation.
+     * This flow provides a read-only view of [_tracks], allowing the UI to reactively display the current
+     * list of [Track] objects as they are updated.
+     */
+    val tracks: StateFlow<List<Track>> = _tracks.asStateFlow()
+
+    init {
+        getAllTracks()
+    }
+
+    /**
+     * Fetches all tracks from the database and updates the [_tracks] state flow.
+     * Launches a coroutine in [viewModelScope] to collect the [Flow] of [TrackDomain] objects from
+     * [ITrackUseCases.getAllTracksUseCase], converts them to presentation [Track] objects using
+     * [List<TrackDomain>.toPresentation], and updates the [_tracks] value for UI consumption.
+     */
+    private fun getAllTracks() {
+        viewModelScope.launch {
+            trackUseCases.getAllTracksUseCase()
+                .collect { listOfTrackDomain ->
+                    _tracks.value = listOfTrackDomain.toPresentation()
+                }
+        }
+    }
+}
