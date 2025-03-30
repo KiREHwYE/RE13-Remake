b8f36fefeec0b9b0a42c13947c44700e0af8215c
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-03-30 13:05:52 +0300

    feat(feature:home): add HomeViewModel provider to HomeComponent.

--- feature/home/src/main/java/com/kire/home/di/HomeComponent.kt
+++ feature/home/src/main/java/com/kire/home/di/HomeComponent.kt
@@ -4,14 +4,15 @@ import com.kire.database.di.DatabaseComponent
 import com.kire.di.CoroutineModule
 import com.kire.home.domain.repository.ITrackRepository
 import com.kire.home.domain.usecase.ITrackUseCases
+import com.kire.home.presentation.viewmodel.HomeViewModel
 import dagger.Component
 import javax.inject.Singleton
 
 /**
  * Dagger component providing dependencies for the home feature's track management functionality.
  * This singleton component integrates [HomeModule] and [CoroutineModule] for module-provided dependencies
- * and depends on [DatabaseComponent] for database-related dependencies, offering access to [ITrackRepository]
- * and [ITrackUseCases] implementations. It ensures a single instance of provided objects throughout the
+ * and depends on [DatabaseComponent] for database-related dependencies, offering access to [ITrackRepository],
+ * [ITrackUseCases] and [HomeViewModel] implementations. It ensures a single instance of provided objects throughout the
  * application lifecycle, facilitating track-related operations in the home feature.
  *
  * @author Michael Gontarev (KiREHwYE)
@@ -33,4 +34,11 @@ interface HomeComponent {
      * @return The [ITrackUseCases] implementation, typically a [TrackUseCases].
      */
     fun provideTrackUseCases(): ITrackUseCases
+
+    /**
+     * Provides a [HomeViewModel] instance for the home feature UI.
+     *
+     * @return The [HomeViewModel] instance.
+     */
+    fun provideHomeViewModel(): HomeViewModel
 }
