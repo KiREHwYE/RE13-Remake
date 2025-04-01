65b74a9362585b9848485b429c42cd9a49319c69
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-04-01 20:31:56 +0300

    feat(core:data): add DataComponent
    Dagger component that provides data-related dependencies in the :core:data module. This component exposes the DatabaseUpdater instance

---
+++ core/data/src/main/java/com/kire/data/di/DataComponent.kt
@@ -0,0 +1,24 @@
+package com.kire.data.di
+
+import com.kire.data.DatabaseUpdater
+import dagger.Component
+
+/**
+ * Dagger component that provides data-related dependencies in the `:core:data` module.
+ * This component exposes the [DatabaseUpdater] instance for synchronizing audio file metadata
+ * with the local Room database, relying on dependencies defined in [DataModule] and
+ * optionally other modules like [DatabaseModule] from `:core:database`.
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+@Component
+interface DataComponent {
+
+    /**
+     * Provides an instance of [DatabaseUpdater] for updating the local Room database with
+     * audio file metadata retrieved from the device's MediaStore.
+     *
+     * @return The [DatabaseUpdater] instance configured with dependencies from [DataModule].
+     */
+    fun provideDatabaseUpdater(): DatabaseUpdater
+}
