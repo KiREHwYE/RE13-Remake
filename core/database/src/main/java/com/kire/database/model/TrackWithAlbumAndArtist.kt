2c897aa58d122c6d6c6ac7a90dc40fc62ec1db95
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-04-02 19:12:31 +0300

    feat(core:database): replace @Relation with flat TrackWithAlbumAndArtist values

--- core/database/src/main/java/com/kire/database/model/TrackWithAlbumAndArtist.kt
+++ core/database/src/main/java/com/kire/database/model/TrackWithAlbumAndArtist.kt
@@ -1,7 +1,5 @@
 package com.kire.database.model
 
-import androidx.room.Embedded
-import androidx.room.Relation
 import com.kire.database.model.entity.AlbumEntity
 import com.kire.database.model.entity.ArtistEntity
 import com.kire.database.model.entity.TrackEntity
@@ -10,22 +8,30 @@ import com.kire.database.model.entity.TrackEntity
  * Represents a track with its associated album and artist details retrieved from the database.
  * This entity combines data from [TrackEntity], [AlbumEntity], and [ArtistEntity].
  *
- * @param track The [TrackEntity] instance.
- * @param album The [AlbumEntity] instance.
- * @param artist The [ArtistEntity] instance.
+ * @param id Unique identifier of the audio file. Defaults to 0 if not specified.
+ * @param title Title of the audio file.
+ * @param albumTitle Title of the album.
+ * @param artistName What the artist calls himself.
+ * @param lyrics Lyrics of the audio file, nullable and defaults to null.
+ * @param duration Duration in milliseconds.
+ * @param dateModified Last modification timestamp in seconds since epoch.
+ * @param bitrate Bitrate in bits per second.
+ * @param size Size in bytes.
+ * @param data URI in String format to the audio file's location.
+ * @param artworkUri URI in String format to the album artwork.
  *
  * @author Michael Gontarev (KiREHwYE)
  */
 data class TrackWithAlbumAndArtist(
-    @Embedded val track: TrackEntity,
-    @Relation(
-        parentColumn = "albumId",
-        entityColumn = "id"
-    )
-    val album: AlbumEntity,
-    @Relation(
-        parentColumn = "artistId",
-        entityColumn = "id"
-    )
-    val artist: ArtistEntity
+    val id: Long,
+    val title: String,
+    val albumTitle: String,
+    val artistName: String,
+    val duration: Long,
+    val lyrics: String? = null,
+    val dateModified: Long,
+    val bitrate: Long,
+    val size: Long,
+    val data: String,
+    val artworkUri: String,
 )
