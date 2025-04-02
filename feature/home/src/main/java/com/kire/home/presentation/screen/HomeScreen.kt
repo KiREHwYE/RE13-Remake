a7827cd82eada378f51dd16771230d194f91d58a
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-04-02 20:21:32 +0300

    feat(feature:home): add HomeScreen composable for displaying track list
    Introduced HomeScreen composable in :feature:home, displaying a scrollable list of tracks using LazyColumn and BaseListItem. Integrated with HomeViewModel to fetch tracks reactively via Flow

---
+++ feature/home/src/main/java/com/kire/home/presentation/screen/HomeScreen.kt
@@ -0,0 +1,52 @@
+package com.kire.home.presentation.screen
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.kire.home.presentation.viewmodel.HomeViewModel
+import com.kire.ui.Dimens.Padding.vertical1
+import com.kire.ui.composable.BaseListItem
+
+/**
+ * A composable screen that displays a list of tracks.
+ * This screen uses a [LazyColumn] , with each track represented by a [BaseListItem].
+ * The track data is fetched reactively from [HomeViewModel] and
+ * updates automatically when the underlying data changes.
+ *
+ * @param homeViewModel The [HomeViewModel] instance providing the track data as a [Flow] of
+ *                      [Track] objects.
+ *
+ * @author Michael Gontarev (KiREHwYE)
+ */
+@Composable
+fun HomeScreen(
+    homeViewModel: HomeViewModel
+) {
+    val tracks by homeViewModel.tracks.collectAsStateWithLifecycle()
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = PaddingValues(vertical = vertical1)
+    ) {
+        items(tracks, key = { track -> track.id} ) { track ->
+
+            BaseListItem(
+                primaryText = track.title,
+                secondaryText = track.artistName + "-" + track.albumTitle,
+                artwork = track.artworkUri,
+                onBaseListItemClick = {
+                    // TODO
+                },
+                onActionButtonClick = {
+                    // TODO
+                }
+            )
+        }
+    }
+}
