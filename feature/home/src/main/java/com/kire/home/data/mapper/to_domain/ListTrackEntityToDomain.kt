af2cfad4da63b5feed7b19b5f2c43300f9474ddb
KiREHwYE <128463806+KiREHwYE@users.noreply.github.com>
2025-04-02 19:24:45 +0300

    feat(feature:home): update mapper for list of TrackWithAlbumAndArtist

--- feature/home/src/main/java/com/kire/home/data/mapper/to_domain/ListTrackEntityToDomain.kt
+++ feature/home/src/main/java/com/kire/home/data/mapper/to_domain/ListTrackEntityToDomain.kt
@@ -1,18 +1,18 @@
 package com.kire.home.data.mapper.to_domain
 
-import com.kire.database.model.entity.TrackEntity
+import com.kire.database.model.TrackWithAlbumAndArtist
 
 /**
- * Converts a list of [TrackEntity] objects from the data layer into a list of [TrackDomain] objects for the domain layer.
- * This extension function applies the [TrackEntity.toDomain] mapping to each element in the list,
+ * Converts a list of [TrackWithAlbumAndArtist] objects from the data layer into a list of [TrackDomain] objects for the domain layer.
+ * This extension function applies the [TrackWithAlbumAndArtist.toDomain] mapping to each element in the list,
  * providing a convenient way to transform an entire collection of database entities into platform-independent
  * domain models for use in business logic.
  *
- * @receiver The list of [TrackEntity] instances to be converted.
- * @return A list of [TrackDomain] instances, each mapped from a corresponding [TrackEntity] in this list.
+ * @receiver The list of [TrackWithAlbumAndArtist] instances to be converted.
+ * @return A list of [TrackDomain] instances, each mapped from a corresponding [TrackWithAlbumAndArtist] in this list.
  *
  * @author Michael Gontarev (KiREHwYE)
  */
-fun List<TrackEntity>.toDomain() = this.map { trackEntity ->
-    trackEntity.toDomain()
+fun List<TrackWithAlbumAndArtist>.toDomain() = this.map { TrackWithAlbumAndArtist ->
+    TrackWithAlbumAndArtist.toDomain()
 }
